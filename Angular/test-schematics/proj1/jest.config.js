ad7a55c6857af71ce8cd871ffa0748bfd28c229e
Marco Antonio Arias Mesia <mariasme@everis.com>
2021-02-26 20:22:11 -0500

    add shematics angular everis

---
+++ Angular/test-schematics/proj1/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    preset: "jest-puppeteer",
+    globals: {
+      URL: "http://localhost:8080"
+    },
+    testMatch: [
+      "./tests/**.test.js"
+    ],
+    verbose: true
+  };
