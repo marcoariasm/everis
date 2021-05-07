3aa895a1c1c9c06e09149e865f2eb49efc473b97
marco.arias.mesia <marco.arias.mesia@gmail.com>
2021-05-07 04:24:07 -0500

    feat angular service worker

--- Angular/udemy/6-formulariosApp/src/app/reactive/dinamicos/dinamicos.component.ts
+++ Angular/udemy/6-formulariosApp/src/app/reactive/dinamicos/dinamicos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dinamicos',
@@ -9,10 +10,39 @@ import { Component, OnInit } from '@angular/core';
 export class DinamicosComponent implements OnInit {
 
   a: number =0;
+  miFormulario: FormGroup = this.formBuilder.group({
+    nombre     : ['', [Validators.required, Validators.minLength(3)] ],
+    favoritos  : this.formBuilder.array( [
+      ['ratita', Validators.required],
+      ['palito queca', Validators.required]
+    ], Validators.required ),
+  })
 
-  constructor() { }
+  constructor( private formBuilder: FormBuilder ) { }
 
   ngOnInit(): void {
+    this.miFormulario.reset({
+      nombre: '',
+      juego: [
+        {id:1,nombre:'ratita peluchito en su palito'},
+        {id:1,nombre:'palito con tira de pompón'}
+      ],
+    })
+  }
+
+  campoEsValido(campo: string): boolean {
+    return this.miFormulario.controls[campo].errors! &&
+            this.miFormulario.controls[campo].touched
+  }
+
+  guardar() {
+    if (this.miFormulario.invalid){
+      this.miFormulario.markAllAsTouched();
+      return;
+    }
+    console.log(this.miFormulario.value)
+    
+
   }
 
 }
